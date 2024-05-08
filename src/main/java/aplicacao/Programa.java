a51778e725ce2c7a93b8f3cb3551df811f3c3b32
Gumierow <guilherme.gumiero@outlook.com>
2024-05-08 00:18:35 -0300

    Mapeamentos do JPA, EntityManager e EntityManagerFactory

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,20 +1,36 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
 
-		Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
 
-		Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
 
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		Pessoa p = em.find(Pessoa.class, 7);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+
+		em.close();
+		emf.close();
+
+		System.out.println("Pronto");
 	}
-
 }
